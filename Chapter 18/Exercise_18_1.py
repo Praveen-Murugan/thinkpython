0acb826f7a4be48815e757c135d23f5c67fcecf7
Praveen-Murugan <76646549+Praveen-Murugan@users.noreply.github.com>
2020-12-30 17:27:35 +0530

    Solutions Uploaded

---
+++ Chapter 18/Exercise_18_1.py
@@ -0,0 +1,26 @@
+"""
+Write a __cmp__ method for Time objects. Hint: you can use tuple comparison, but
+you also might consider using integer subtraction
+
+"""
+class Time(object):
+    def __init__(self, hour=0, minute=0):
+        self.hour = hour
+        self.minute = minute
+
+    def __lt__(self, other):
+      return (self.hour, self.minute) < (other.hour, other.minute)
+
+    def __gt__(self, other):
+      return (self.hour, self.minute) > (other.hour, other.minute)
+
+    def __eq__(self, other):
+      return (self.hour, self.minute) == (other.hour, other.minute)
+
+    def __repr__(self):
+      return '{}'.format((self.hour, self.minute))
+
+a = Time(hour=3, minute=31)
+b = Time(hour=4, minute=30)
+
+print(a < b)
---
+++ Chapter 18/Exercise_18_2.py
@@ -0,0 +1,31 @@
+"""
+Write a Deck method named sort that uses the list method sort to sort the cards
+in a Deck. sort uses the __cmp__ method we defined to determine sort order
+
+"""
+class Deck(object):
+
+    def __init__(self,cards):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return "\n".join(res)
+
+    def pop_card(self):
+        return self.cards.pop()
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def sort(self):
+        self.cards.sort()
